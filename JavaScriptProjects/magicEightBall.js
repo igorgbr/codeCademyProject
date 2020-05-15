230ad521e5bba910984b7fc31a5d90e1d28999a3
igorgbr <igorgphotoarte@gmail.com>
2020-05-15 00:44:54 -0300

    project Magic Eight Ball

---
+++ JavaScriptProjects/magicEightBall.js
@@ -0,0 +1,39 @@
+let userName = 'igor';
+userName !== null ? console.log(`Hello ${userName}`) : console.log(`Hello`);
+console.log(`--------------------\n`)
+let userQuestion = 'I can be a x-men?';
+console.log(`${userName} asked: ${userQuestion}`);
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = '';
+
+switch (randomNumber) {
+  case 0: 
+    eightBall = 'It is certain';
+    break;
+  case 1: 
+    eightBall = 'It is decidedly so';
+    break;
+  case 2: 
+    eightBall = 'Reply hazy try again';
+    break;
+  case 3: 
+    eightBall = 'Cannot predict now';
+    break;
+  case 4: 
+    eightBall = 'Do not count on it';
+    break;
+  case 5: 
+    eightBall = 'My sources say no';
+    break;
+  case 6: 
+    eightBall = 'Outlook not so good';
+    break;
+  case 7: 
+    eightBall = 'Signs point to yes';
+    break;
+  case 8: 
+    eightBall = 'It is certain';
+    break;
+}
+
+console.log(`${eightBall}`);
