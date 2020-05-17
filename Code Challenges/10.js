7474f81a7bfe5807a39b61c9f7ae24502e900541
igorgbr <igorgphotoarte@gmail.com>
2020-05-17 03:41:39 -0300

    exercises 6 - 10 ok

---
+++ Code Challenges/10.js
@@ -0,0 +1,9 @@
+// Write a function, sillySentence(), that has 3 string 
+// parameters and returns the following silly sentence with the blanks filled in by the 
+// arguments passed into the function
+
+// Write your function here:
+const sillySentence = (adj, verb, noum) => `I am so ${adj} because I ${verb} coding! Time to write some more awesome ${noum}!`;
+
+// Uncomment the line below when you're ready to try out your function
+console.log(sillySentence('excited', 'love', 'functions')) 
